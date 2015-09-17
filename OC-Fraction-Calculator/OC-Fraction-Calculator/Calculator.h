cca48c7c3a5df1ed738fabcf580669f85486a72b
lzcuriosity <476194489@qq.com>
2015-09-17 12:22:54 +0800

    Calculator 类更新

--- OC-Fraction-Calculator/OC-Fraction-Calculator/Calculator.h
+++ OC-Fraction-Calculator/OC-Fraction-Calculator/Calculator.h
@@ -7,7 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Fraction.h"
 
 @interface Calculator : NSObject
+
+@property (strong, nonatomic) Fraction *operand1;
+@property (strong, nonatomic) Fraction *operand2;
+@property (strong, nonatomic) Fraction *accumulator;
+
+-(Fraction *) performOperation: (char) op;
+-(void) clear;
 
 @end
